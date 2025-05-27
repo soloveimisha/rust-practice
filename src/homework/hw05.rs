ea7963e2678804197407e4d855f65ea4e0bf4f2a
soloveimisha <mykhailo.solovei@e-u.edu.ua>
2025-05-27 22:37:02 +0300

    Update hw05.rs

---
+++ src/homework/hw05.rs
@@ -0,0 +1,34 @@
+pub fn gcd(mut a: u32, mut b: u32) -> u32 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let data = [
+            ((24,  60), 12),
+            ((15,   9),  3),
+            ((15,   6),  3),
+            ((140, 40), 20),
+            ((24,  16),  8),
+            ((100, 10), 10),
+            ((120, 80), 40),
+            ((80, 120), 40),
+            ((100, 20), 20),
+            ((37,  11),  1),
+            ((120, 90), 30),
+        ];
+
+        for ((a, b), exp) in data.iter() {
+            assert_eq!(*exp, gcd(*a, *b));
+        }
+    }
+}
